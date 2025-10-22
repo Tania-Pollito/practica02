ab5875480b5be420cdaad795301a23d99d7f981e
“Tania <taniamorenomorales@gmail.com>
2025-10-22 13:50:57 -0500

    Implementado actualización de tasas. Close #1

--- conversor.py
+++ conversor.py
@@ -1,4 +1,5 @@
 import json
+import random
 from datetime import datetime
 def cargar_tasas(ruta):
      """ lee un archivo json y retorna un objeto """
@@ -18,8 +19,20 @@ def registrar_transaccion(producto, precio_convertido, moneda, ruta_log):
           fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
           #Escribir una línea nueva en el archivo de registro
           archivo.write(f"{fecha} | {producto}: {precio_convertido:.2f} {moneda}\n")
+def actualizar_tasas(ruta):
+      # Simular API: Cambiar tasas aleatoriamente ±2%
+     with open(ruta, "r+") as archivo:
+          tasas = json.load(archivo)
+          for moneda in tasas["USD"]:
+               tasas["USD"][moneda] *= 0.98 + (0.04 * random.random())
+          tasas["actualizacion"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+          archivo.seek(0)
+          json.dump(tasas, archivo, indent=2)          
+
 # Ejemplo de uso
 if __name__ == "__main__":
+     #actualizar las tasas
+     actualizar_tasas("data/tasas.json")
      tasas = cargar_tasas("data/tasas.json")
      precio_usd = 100.00
      precio_eur = convertir(precio_usd, "EUR", tasas)
